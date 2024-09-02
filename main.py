59f3dca33c64449249ae36cc924d16478c4e7624
Criallis <54024220+RuslanSad@users.noreply.github.com>
2024-09-02 23:13:02 +0300

    pishet privet

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет! Я ваш бот.')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('7482905333:AAGxcbHnucIczwgSgBUid6Rb6FSlIrXaYJk').build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("echo", echo))
+    application.run_polling()
